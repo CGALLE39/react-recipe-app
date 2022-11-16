46afc440c47b0f16dd6cc805107080035213e821
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-11-16 14:04:34 -0600

    backend

---
+++ src/main/java/com/example/reactfoodapp/ReactFoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactfoodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactFoodAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactFoodAppApplication.class, args);
+    }
+
+}
